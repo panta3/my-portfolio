27e00560a6f47e7980f0c230ee2b5f62b3f031f3
panta3 <aaravpant00@gmail.com>
2023-10-31 14:14:31 -0400

    Dynamic date change in skill section using js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addRecommendation() {
   if (trimmedValue !== "") {
     console.log("New recommendation added");
     // Call showPopup here
-    showPopup(true);
+    showPopup(true, "Thanks for leaving a recommendation");
 
     // Create a new 'recommendation' element and set its value to the user's message
     var element = document.createElement("div");
@@ -25,17 +25,53 @@ function addRecommendation() {
     // Display an error message or highlight the input field
     console.log("Recommendation message is empty");
     // You can add code here to display an error message to the user or style the input field to indicate an error
+    showPopup(false, "Recommendation message is empty");
   }
 }
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible';
-    // Hide the popup after a certain time (e.g., 3 seconds)
-    setTimeout(() => {
-      showPopup(false);
-    }, 3000);
+function showPopup(isSuccess, message) {
+  const popup = document.getElementById('popup');
+  const h3 = popup.querySelector('h3');
+  
+  if (isSuccess) {
+    popup.style.backgroundColor = '#e8bcf0';
   } else {
-    document.getElementById('popup').style.visibility = 'hidden';
+    popup.style.backgroundColor = '#f8d7da';
   }
+  
+  h3.textContent = message;
+  popup.style.visibility = 'visible';
+  
+  // Hide the popup after a certain time (e.g., 3 seconds)
+  setTimeout(() => {
+    popup.style.visibility = 'hidden';
+  }, 3000);
 }
+
+function updateExperience() {
+  const skills = document.querySelectorAll('.skill');
+  skills.forEach(skill => {
+    const startDate = skill.dataset.startDate;
+    if (startDate) {
+      const start = new Date(startDate);
+      const now = new Date();
+      const months = (now.getFullYear() - start.getFullYear()) * 12 + now.getMonth() - start.getMonth();
+      const years = Math.floor(months / 12);
+      const remainingMonths = months % 12;
+      let experience = '';
+      if (years > 0) {
+        experience += years === 1 ? '1 year ' : `${years} years `;
+      }
+      if (remainingMonths > 0) {
+        experience += remainingMonths === 1 ? '1 month' : `${remainingMonths} months`;
+      }
+      if (experience === '') {
+        experience = 'Less than a month';
+      }
+      skill.querySelector('.experience').textContent = experience;
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', updateExperience);
+
